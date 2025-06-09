02668df8447f60baa1a1ff230634f7c47a8dc980
Prakash <mprakash4678@gmail.com>
2025-06-09 23:33:29 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Main from "./components/Main/Main"
+import SideBar from "./components/Sidebar/SideBar"
+
+const App = () => {
+  return (
+    <>
+
+    <SideBar />
+    <Main />
+    
+    </>
+  )
+}
+export default App
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,69 @@
+import { useContext } from "react";
+import { assets } from "../../assets/assets";
+import "./Main.css";
+import { Context } from "../../context/Context";
+
+const Main = () => {
+  const {
+    onSent,
+    recentPrompt,
+    showResult,
+    loading,
+    resultData,
+    setInput,
+    input,
+  } = useContext(Context);
+
+  return (
+    <div className="main">
+      <div className="nav">
+        <p>Gemini</p>
+        <img src={assets.user_icon} alt="" />
+      </div>
+      <div className="main-container">
+        {!showResult ? (
+          <>
+            <div className="greet">
+              <span>Hello, Dev</span>
+            </div>
+          </>
+        ) : (
+          <div className="result">
+            <div className="result-title">
+              <p>{recentPrompt}</p>
+            </div>
+            <div className="result-data">
+              <img src={assets.gemini_icon} alt="" />
+              {loading ? (
+                <div className="loader">
+                  <hr />
+                  <hr />
+                  <hr />
+                </div>
+              ) : (
+                <p dangerouslySetInnerHTML={{ __html: resultData }}></p>
+              )}
+            </div>
+          </div>
+        )}
+
+        <div className="main-bottom">
+          <div className="search-box">
+            <input
+              onChange={(e) => setInput(e.target.value)}
+              value={input}
+              type="text"
+              placeholder="Ask Gemini"
+            />
+            <div>
+              <img src={assets.gallery_icon} alt="" />
+              <img src={assets.mic_icon} alt="" />
+              {input ? <img onClick={() => onSent()} src={assets.send_icon} alt="" /> : null}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Main;
